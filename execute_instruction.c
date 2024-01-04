bf1a59a42f3f57d733cae2a626b76736b039b7bd
HB74C21 <6720@holbertonstudents.com>
2024-01-04 14:16:52 +0000

    delete commentary

--- execute_instruction.c
+++ execute_instruction.c
@@ -23,23 +23,19 @@ void execute_instruction(char *instruction, stack_t **stack,
 	{NULL, NULL}
 };
 
-	/* Tokenisation de la chaîne d'instructions */
 	opcode = strtok(instruction, " \t\n");
 	if (!opcode || opcode[0] == '#')
 		return;
 
-	/* Recherche de l'opcode dans le tableau d'opcodes */
 	for (i = 0; opcodes[i].opcode; i++)
 	{
 		if (strcmp(opcode, opcodes[i].opcode) == 0)
 		{
-			/* Opcode trouvé, appeler la fonction correspondante */
 			opcodes[i].f(stack, line_number, instruction);
 			return;
 		}
 	}
 
-	/* Si l'opcode n'est pas trouvé, afficher un message d'erreur */
 	fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
 	exit(EXIT_FAILURE);
 }
